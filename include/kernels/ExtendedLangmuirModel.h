e979fc7ae284634b4e27d9a94307af83775eb54f
Austin Ladshaw <aladshaw3@outlook.com>
2020-03-10 11:40:39 -0400

    Added temperature coupling to the langmuir function. This is needed in order to simulate TPDs

---
+++ include/kernels/ExtendedLangmuirModel.h
@@ -0,0 +1,102 @@
+/*!
+ *  \file ExtendedLangmuirModel.h
+ *	\brief Standard kernel for coupling a vector non-linear variables via an extended langmuir function
+ *	\details This file creates a standard MOOSE kernel for the coupling of a vector non-linear variables
+ *			together via an extended langmuir forcing function,
+ *			i.e., variable = b_i * K_i * coupled_variable_i / 1 + sum(j, K_j * coupled_variable_j).
+ *			In this kernel, the langmuir coefficients {K_i} are calculated as a function of temperature
+ *			using the van't Hoff expression: ln(K_i) = -dH_i/(R*T) + dS_i/R
+ *
+ *  \note This should be used in conjunction with a TimeDerivative or Reaction kernel inside of the
+ *        input file to enfore that the variable sorbs material according to Langmuir
+ *
+ *        Reaction kernel ==>   Res(u) = u*test
+ *        Extended Langmuir ==> Res(u) = -ExtLangFunc*test
+ *
+ *  \author Austin Ladshaw
+ *	\date 03/10/2020
+ *	\copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in catalyst
+ *              performance for new vehicle technologies.
+ *
+ *			   Austin Ladshaw does not claim any ownership or copyright to the
+ *			   MOOSE framework in which these kernels are constructed, only
+ *			   the kernels themselves. The MOOSE framework copyright is held
+ *			   by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#pragma once
+
+#include "ExtendedLangmuirFunction.h"
+
+#ifndef Rstd
+#define Rstd 8.3144621						///< Gas Constant in J/K/mol (or) L*kPa/K/mol (Standard Units)
+#endif
+
+/// This macro calculates the natural log of the dimensionless isotherm parameter
+#ifndef lnKo
+#define lnKo(H,S,T)	-( H / ( Rstd * T ) ) + ( S / Rstd )
+#endif
+
+/// ExtendedLangmuirModel class object forward declarationss
+class ExtendedLangmuirModel;
+
+template<>
+InputParameters validParams<ExtendedLangmuirModel>();
+
+/// ExtendedLangmuirModel class object inherits from ExtendedLangmuirFunction object
+/** This class object inherits from the CoupledExtendedLangmuirFunction object in the MOOSE framework.
+	All public and protected members of this class are required function overrides.
+	The kernel interfaces the set of non-linear variables to couple an extended Langmuir
+	forcing function between given objects. */
+class ExtendedLangmuirModel : public ExtendedLangmuirFunction
+{
+public:
+	/// Required constructor for objects in MOOSE
+	ExtendedLangmuirModel(const InputParameters & parameters);
+
+protected:
+	/// Function to compute all langmuir coefficients from temperature
+	void computeAllLangmuirCoeffs();
+
+	/// Function to compute the Jacobi for the coupled temperature
+	Real computeExtLangmuirTempJacobi();
+
+	/// Required residual function for standard kernels in MOOSE
+	/** This function returns a residual contribution for this object.*/
+	virtual Real computeQpResidual();
+
+	/// Required Jacobian function for standard kernels in MOOSE
+	/** This function returns a Jacobian contribution for this object. The Jacobian being
+	 computed is the associated diagonal element in the overall Jacobian matrix for the
+	 system and is used in preconditioning of the linear sub-problem. */
+	virtual Real computeQpJacobian();
+
+	/// Not Required, but aids in the preconditioning step
+	/** This function returns the off diagonal Jacobian contribution for this object. By
+	 returning a non-zero value we will hopefully improve the convergence rate for the
+	 cross coupling of the variables. */
+	virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+	std::vector<Real> _enthalpies;						///< Vector of enthalpies for all langmuir coefficients (J/mol)
+	std::vector<Real> _entropies;						///< Vector of entropies for all langmuir coefficients (J/K/mol)
+	const VariableValue & _coupled_temp;				///< Coupled variable for temperature
+	const unsigned int _coupled_var_temp;				///< Index for the coupled temperature variable
+
+private:
+
+};
--- src/kernels/ExtendedLangmuirModel.C
+++ src/kernels/ExtendedLangmuirModel.C
@@ -1,9 +1,11 @@
 /*!
- *  \file ExtendedLangmuirFunction.h
+ *  \file ExtendedLangmuirModel.h
  *	\brief Standard kernel for coupling a vector non-linear variables via an extended langmuir function
  *	\details This file creates a standard MOOSE kernel for the coupling of a vector non-linear variables
  *			together via an extended langmuir forcing function,
  *			i.e., variable = b_i * K_i * coupled_variable_i / 1 + sum(j, K_j * coupled_variable_j).
+ *			In this kernel, the langmuir coefficients {K_i} are calculated as a function of temperature
+ *			using the van't Hoff expression: ln(K_i) = -dH_i/(R*T) + dS_i/R
  *
  *  \note This should be used in conjunction with a TimeDerivative or Reaction kernel inside of the
  *        input file to enfore that the variable sorbs material according to Langmuir
@@ -37,32 +39,31 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-#include "ExtendedLangmuirFunction.h"
+#include "ExtendedLangmuirModel.h"
 
-
-registerMooseObject("catsApp", ExtendedLangmuirFunction);
+registerMooseObject("catsApp", ExtendedLangmuirModel);
 
 template<>
-InputParameters validParams<ExtendedLangmuirFunction>()
+InputParameters validParams<ExtendedLangmuirModel>()
 {
-	InputParameters params = validParams<Kernel>();
-	params.addParam<Real>("site_density",0.0,"Maximum Capacity for Langmuir Function of this sorption site (mol/L)");
-	params.addParam< std::vector<Real> >("langmuir_coeff","Coefficient for the langmuir function (L/mol)");
-	params.addRequiredCoupledVar("coupled_list","List of names of the variables being coupled");
-	params.addRequiredCoupledVar("main_coupled","Name of the primary variable being coupled");
+	InputParameters params = validParams<ExtendedLangmuirFunction>();
+	params.addParam< std::vector<Real> >("enthalpies","Enthalpies for the Langmuir Coefficients (J/mol)");
+	params.addParam< std::vector<Real> >("entropies","Entropies for the Langmuir Coefficients (J/K/mol)");
+	params.addRequiredCoupledVar("coupled_temp","Name of the coupled temperature variable");
 	return params;
 }
 
-ExtendedLangmuirFunction::ExtendedLangmuirFunction(const InputParameters & parameters)
-: Kernel(parameters),
-_maxcap(getParam<Real>("site_density")),
-_langmuir_coef(getParam<std::vector<Real> >("langmuir_coeff")),
-_coupled_i(coupledValue("main_coupled")),
-_coupled_var_i(coupled("main_coupled"))
+ExtendedLangmuirModel::ExtendedLangmuirModel(const InputParameters & parameters)
+: ExtendedLangmuirFunction(parameters),
+_enthalpies(getParam<std::vector<Real> >("enthalpies")),
+_entropies(getParam<std::vector<Real> >("entropies")),
+_coupled_temp(coupledValue("coupled_temp")),
+_coupled_var_temp(coupled("coupled_temp"))
 {
 	unsigned int n = coupledComponents("coupled_list");
 	_coupled_vars.resize(n);
 	_coupled.resize(n);
+	_langmuir_coef.resize(n);
 
 	for (unsigned int i = 0; i<_coupled.size(); ++i)
 	{
@@ -71,67 +72,61 @@ _coupled_var_i(coupled("main_coupled"))
 		if (_coupled_vars[i] == _coupled_var_i)
 			_lang_index = i;
 	}
-
 }
 
-Real ExtendedLangmuirFunction::computeExtLangmuirEquilibrium()
+void ExtendedLangmuirModel::computeAllLangmuirCoeffs()
 {
-	double sum = 0.0;
 	for (unsigned int i = 0; i<_coupled.size(); ++i)
 	{
-		sum = sum + _langmuir_coef[i] * (*_coupled[i])[_qp];
+		_langmuir_coef[i] = std::exp( lnKo(_enthalpies[i], _entropies[i], _coupled_temp[_qp]) );
 	}
-	return _maxcap*((_langmuir_coef[_lang_index]*_coupled_i[_qp])/(1.0+sum));
 }
 
-Real ExtendedLangmuirFunction::computeExtLangmuirConcJacobi()
+Real ExtendedLangmuirModel::computeExtLangmuirTempJacobi()
 {
-	double sum = 0.0;
+	double kc_sum = 0.0, kch_sum = 0.0;
 	for (unsigned int j = 0; j<_coupled.size(); ++j)
 	{
-		sum = sum + _langmuir_coef[j] * (*_coupled[j])[_qp];
+		kc_sum = kc_sum + _langmuir_coef[j] * (*_coupled[j])[_qp];
+		kch_sum = kch_sum + _langmuir_coef[j] * (*_coupled[j])[_qp] * _enthalpies[j];
 	}
-	double numerator = _langmuir_coef[_lang_index]*_phi[_j][_qp] * (1.0 + sum - _langmuir_coef[_lang_index]*_coupled_i[_qp]);
-	double denom = (1.0+sum)*(1.0+sum);
-	return _maxcap*numerator/denom;
+	double numerator = _enthalpies[_lang_index] + (_enthalpies[_lang_index]*kc_sum) - kch_sum;
+	double denom = (1.0+kc_sum)*(1.0+kc_sum);
+	return _maxcap*_langmuir_coef[_lang_index]*_coupled_i[_qp]*_phi[_j][_qp]*(1.0/Rstd/_coupled_temp[_qp]/_coupled_temp[_qp])*numerator/denom;
 }
 
-Real ExtendedLangmuirFunction::computeExtLangmuirOffJacobi(int i)
+Real ExtendedLangmuirModel::computeQpResidual()
 {
-	double sum = 0.0;
-	for (unsigned int j = 0; j<_coupled.size(); ++j)
-	{
-		sum = sum + _langmuir_coef[j] * (*_coupled[j])[_qp];
-	}
-	double numerator = -_langmuir_coef[_lang_index]*_coupled_i[_qp]*_phi[_j][_qp]*_langmuir_coef[i];
-	double denom = (1.0+sum)*(1.0+sum);
-	return _maxcap*numerator/denom;
-
+	computeAllLangmuirCoeffs();
+	return ExtendedLangmuirFunction::computeQpResidual();
 }
 
-Real ExtendedLangmuirFunction::computeQpResidual()
+Real ExtendedLangmuirModel::computeQpJacobian()
 {
-	return -_test[_i][_qp]*computeExtLangmuirEquilibrium();
+	return ExtendedLangmuirFunction::computeQpJacobian();
 }
 
-Real ExtendedLangmuirFunction::computeQpJacobian()
+Real ExtendedLangmuirModel::computeQpOffDiagJacobian(unsigned int jvar)
 {
-	return 0.0;
-}
+	computeAllLangmuirCoeffs();
 
-Real ExtendedLangmuirFunction::computeQpOffDiagJacobian(unsigned int jvar)
-{
+	//Off-diagonals for non-main concentrations
 	for (unsigned int i = 0; i<_coupled.size(); ++i)
 	{
 		if (jvar == _coupled_vars[i] && jvar != _coupled_var_i)
 		{
-			return -_test[_i][_qp]*computeExtLangmuirOffJacobi(i);
+			return -_test[_i][_qp]*ExtendedLangmuirFunction::computeExtLangmuirOffJacobi(i);
 		}
 	}
-
+	//Off-diagonal for main concentration
 	if (jvar == _coupled_var_i)
 	{
-		return -_test[_i][_qp]*computeExtLangmuirConcJacobi();
+		return -_test[_i][_qp]*ExtendedLangmuirFunction::computeExtLangmuirConcJacobi();
+	}
+	//Off-diagonal for temperature
+	if (jvar == _coupled_var_temp)
+	{
+		return -_test[_i][_qp]*computeExtLangmuirTempJacobi();
 	}
 
 	return 0.0;
