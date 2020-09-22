6865efdf4b527ee1ddcb1919768d2a8d298702c4
Austin Ladshaw <aladshaw3@outlook.com>
2020-09-22 10:44:43 -0400

    Finished the langmuir inhibition function and tested with concentration coupling.

---
+++ include/kernels/LangmuirInhibition.h
@@ -0,0 +1,111 @@
+/*!
+ *  \file LangmuirInhibition.h
+ *    \brief Kernel for creating an inhibition function of a Langmuir form
+ *    \details This file creates a standard MOOSE kernel for the coupling of a vector non-linear variables
+ *            together via a Langmuir forcing function as follows...
+ *            i.e., Res = 1 + sum(i, K_i * coupled_variable_i)
+ *                  where K_i = A*T^B*exp(-E/R/T)
+ *
+ *            T is a coupled temperature and coupled_variable_i are coupled concentrations
+ *
+ *  \note This should be used in conjunction with a Reaction kernel inside of the
+ *        input file to enfore that the inhibition variable value equals this function. The reason it is
+ *        done in this fashion is so that it will be more modular in how the inhibition variable R could
+ *        be represented if multiple instances of these objects are necessary to define its behavior
+ *
+ *        Reaction kernel ==>   Res(R) = R*test
+ *        Langmuir Inhibition ==> Res(R) = -LangmuirInhibition*test
+ *
+ *  \author Austin Ladshaw
+ *    \date 09/22/2020
+ *    \copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in catalyst
+ *              performance for new vehicle technologies.
+ *
+ *               Austin Ladshaw does not claim any ownership or copyright to the
+ *               MOOSE framework in which these kernels are constructed, only
+ *               the kernels themselves. The MOOSE framework copyright is held
+ *               by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#pragma once
+
+#include "Kernel.h"
+
+#ifndef Rstd
+#define Rstd 8.3144621                        ///< Gas Constant in J/K/mol (or) L*kPa/K/mol (Standard Units)
+#endif
+
+/// LangmuirInhibition class object inherits from Kernel object
+/** This class object inherits from the Kernel object in the MOOSE framework.
+    All public and protected members of this class are required function overrides.
+    The kernel interfaces the set of non-linear variables to couple an extended Langmuir
+    forcing function between given objects. */
+class LangmuirInhibition : public Kernel
+{
+public:
+    /// Required new syntax for InputParameters
+    static InputParameters validParams();
+    
+    /// Required constructor for objects in MOOSE
+     LangmuirInhibition(const InputParameters & parameters);
+
+protected:
+    /// Function to compute all langmuir coefficients from temperature
+    void computeAllLangmuirCoeffs();
+
+    /// Function to compute the i-th term in the Langmuir function
+    Real computeLangmuirTerm(int i);
+
+    /// Function to compute the off-diagonal Jacobi for the coupled concentrations
+    Real computeLangmuirConcJacobi(int i);
+    
+    /// Function to compute the i-th term in the Langmuir function's temperature Jacobi
+    Real computeLangmuirTempJacobiTerm(int i);
+    
+    /// Function to compute the Jacobi for the coupled temperature
+    Real computeLangmuirTempJacobi();
+
+    /// Required residual function for standard kernels in MOOSE
+    /** This function returns a residual contribution for this object.*/
+    virtual Real computeQpResidual();
+
+    /// Required Jacobian function for standard kernels in MOOSE
+    /** This function returns a Jacobian contribution for this object. The Jacobian being
+     computed is the associated diagonal element in the overall Jacobian matrix for the
+     system and is used in preconditioning of the linear sub-problem. */
+    virtual Real computeQpJacobian();
+
+    /// Not Required, but aids in the preconditioning step
+    /** This function returns the off diagonal Jacobian contribution for this object. By
+     returning a non-zero value we will hopefully improve the convergence rate for the
+     cross coupling of the variables. */
+    virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+    std::vector<Real> _langmuir_coef;                   ///< Langmuir Coefficients for the coupled variables (units are inverse concentration)
+    std::vector<Real> _pre_exp;                         ///< Pre-exponential factors for Langmuir coefficients
+    std::vector<Real> _beta;                            ///< Beta factors for the Langmuir coefficients
+    std::vector<Real> _act_energy;                      ///< Activation energies for Langmuir coefficients 
+    std::vector<const VariableValue *> _coupled;        ///< Pointer list to the coupled gases (concentration units)
+    std::vector<unsigned int> _coupled_vars;            ///< Indices for the concentration species in the system
+    const VariableValue & _temp;                        ///< Coupled variable for temperature
+    const unsigned int _temp_var;                       ///< Index for the coupled temperature variable
+
+private:
+
+};
+
---
+++ src/kernels/LangmuirInhibition.C
@@ -0,0 +1,184 @@
+/*!
+ *  \file LangmuirInhibition.h
+ *    \brief Kernel for creating an inhibition function of a Langmuir form
+ *    \details This file creates a standard MOOSE kernel for the coupling of a vector non-linear variables
+ *            together via a Langmuir forcing function as follows...
+ *            i.e., Res = 1 + sum(i, K_i * coupled_variable_i)
+ *                  where K_i = A*T^B*exp(-E/R/T)
+ *
+ *            T is a coupled temperature and coupled_variable_i are coupled concentrations
+ *
+ *  \note This should be used in conjunction with a Reaction kernel inside of the
+ *        input file to enfore that the inhibition variable value equals this function. The reason it is
+ *        done in this fashion is so that it will be more modular in how the inhibition variable R could
+ *        be represented if multiple instances of these objects are necessary to define its behavior
+ *
+ *        Reaction kernel ==>   Res(R) = R*test
+ *        Langmuir Inhibition ==> Res(R) = -LangmuirInhibition*test
+ *
+ *  \author Austin Ladshaw
+ *    \date 09/22/2020
+ *    \copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in catalyst
+ *              performance for new vehicle technologies.
+ *
+ *               Austin Ladshaw does not claim any ownership or copyright to the
+ *               MOOSE framework in which these kernels are constructed, only
+ *               the kernels themselves. The MOOSE framework copyright is held
+ *               by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "LangmuirInhibition.h"
+
+
+registerMooseObject("catsApp", LangmuirInhibition);
+
+InputParameters LangmuirInhibition::validParams()
+{
+    InputParameters params = Kernel::validParams();
+    params.addRequiredParam< std::vector<Real> >("pre_exponentials","Pre-exponential terms for the Langmuir coefficients");
+    params.addParam< std::vector<Real> >("betas","Beta terms for the Langmuir coefficients");
+    params.addParam< std::vector<Real> >("activation_energies","Activation energy terms for the Langmuir coefficients");
+    params.addRequiredCoupledVar("coupled_list","List of names of the variables being coupled");
+    params.addRequiredCoupledVar("temperature","Name of the coupled temperature variable (K)");
+    return params;
+}
+ 
+LangmuirInhibition::LangmuirInhibition(const InputParameters & parameters)
+: Kernel(parameters),
+_pre_exp(getParam<std::vector<Real> >("pre_exponentials")),
+_beta(getParam<std::vector<Real> >("betas")),
+_act_energy(getParam<std::vector<Real> >("activation_energies")),
+_temp(coupledValue("temperature")),
+_temp_var(coupled("temperature"))
+{
+    unsigned int n = coupledComponents("coupled_list");
+    _coupled_vars.resize(n);
+    _coupled.resize(n);
+    _langmuir_coef.resize(n);
+    
+    if (_pre_exp.size() != _langmuir_coef.size())
+    {
+        moose::internal::mooseErrorRaw("User is required to provide (at minimum) a list of pre-exponential factors equal to the number of coupled concentrations.");
+    }
+    
+    for (int i=0; i<_pre_exp.size(); i++)
+    {
+        if (_pre_exp[i] < 0)
+            moose::internal::mooseErrorRaw("Pre-exponentials can NOT be negative numbers!");
+    }
+    
+    if (_beta.size() < _langmuir_coef.size())
+    {
+        for (int i=0; i<(_langmuir_coef.size()-_beta.size()); i++)
+        {
+            _beta.push_back(0.0);
+        }
+    }
+    
+    if (_act_energy.size() < _langmuir_coef.size())
+    {
+        for (int i=0; i<(_langmuir_coef.size()-_act_energy.size()); i++)
+        {
+            _act_energy.push_back(0.0);
+        }
+    }
+
+
+    for (unsigned int i = 0; i<_coupled.size(); ++i)
+    {
+        _coupled_vars[i] = coupled("coupled_list",i);
+        _coupled[i] = &coupledValue("coupled_list",i);
+    }
+
+}
+
+void LangmuirInhibition::computeAllLangmuirCoeffs()
+{
+    for (unsigned int i = 0; i<_coupled.size(); ++i)
+    {
+        _langmuir_coef[i] = _pre_exp[i] * std::pow(_temp[_qp], _beta[i]) * std::exp(-_act_energy[i]/Rstd/_temp[_qp]);
+    }
+}
+
+Real LangmuirInhibition::computeLangmuirTerm(int i)
+{
+    Real val = 0.0;
+    if ((*_coupled[i])[_qp] > 0.0)
+        val = _langmuir_coef[i] * (*_coupled[i])[_qp];
+    return val;
+}
+
+Real LangmuirInhibition::computeLangmuirConcJacobi(int i)
+{
+    Real val = 0.0;
+    if ((*_coupled[i])[_qp] > 0.0)
+        val = _langmuir_coef[i] * _phi[_j][_qp];
+    return val;
+}
+
+Real LangmuirInhibition::computeLangmuirTempJacobiTerm(int i)
+{
+    Real val = 0.0;
+    if ((*_coupled[i])[_qp] > 0.0)
+        val = _langmuir_coef[i] * (*_coupled[i])[_qp] * ((_act_energy[i]/Rstd/_temp[_qp]/_temp[_qp]) + (_beta[i]/_temp[_qp])) * _phi[_j][_qp];
+    return val;
+}
+
+Real LangmuirInhibition::computeLangmuirTempJacobi()
+{
+    Real val = 0.0;
+    for (unsigned int i = 0; i<_coupled.size(); ++i)
+    {
+        val += computeLangmuirTempJacobiTerm(i);
+    }
+    return val;
+}
+
+Real LangmuirInhibition::computeQpResidual()
+{
+    computeAllLangmuirCoeffs();
+    Real sum = 1.0;
+    for (unsigned int i = 0; i<_coupled.size(); ++i)
+    {
+        sum += computeLangmuirTerm(i);
+    }
+    return -_test[_i][_qp]*sum;
+}
+
+Real LangmuirInhibition::computeQpJacobian()
+{
+    return 0.0;
+}
+
+Real LangmuirInhibition::computeQpOffDiagJacobian(unsigned int jvar)
+{
+    computeAllLangmuirCoeffs();
+    
+    if (jvar == _temp_var)
+    {
+        return -_test[_i][_qp]*computeLangmuirTempJacobi();
+    }
+    
+    for (unsigned int i = 0; i<_coupled.size(); ++i)
+    {
+        if (jvar == _coupled_vars[i])
+            return -_test[_i][_qp]*computeLangmuirConcJacobi(i);
+    }
+    
+    return 0.0;
+}
