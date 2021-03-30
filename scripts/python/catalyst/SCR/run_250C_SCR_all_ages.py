6ea00fbde6d068d42c861912c933111ce9eac8f9
Austin Ladshaw <ladshawap@ornl.gov>
2021-03-30 16:40:11 -0400

    Added print statements to view bypass or inlet conditions.

--- scripts/python/catalyst/SCR/run_250C_SCR_all_ages.py
+++ scripts/python/catalyst/SCR/run_250C_SCR_all_ages.py
@@ -926,26 +926,36 @@ sim.run_solver()
 
 sim.print_results_of_breakthrough(["NH3","NO","NO2","N2O","O2","N2","H2O"],
                                         "Unaged", "250C", file_name="Unaged_SCR_250C_breakthrough.txt")
+sim.print_results_of_location(["NH3","NO","NO2","N2O","O2","N2","H2O"],
+                                        "Unaged", "250C", 0, file_name="Unaged_SCR_250C_bypass.txt")
 sim.print_results_of_integral_average(["q1","q2a","q2b","q3a","q3b","q3c"],
                                         "Unaged", "250C", file_name="Unaged_SCR_250C_average_ads.txt")
 
 sim.print_results_of_breakthrough(["NH3","NO","NO2","N2O","O2","N2","H2O"],
                                         "2hr", "250C", file_name="2hr_SCR_250C_breakthrough.txt")
+sim.print_results_of_location(["NH3","NO","NO2","N2O","O2","N2","H2O"],
+                                        "2hr", "250C", 0, file_name="2hr_SCR_250C_bypass.txt")
 sim.print_results_of_integral_average(["q1","q2a","q2b","q3a","q3b","q3c"],
                                         "2hr", "250C", file_name="2hr_SCR_250C_average_ads.txt")
 
 sim.print_results_of_breakthrough(["NH3","NO","NO2","N2O","O2","N2","H2O"],
                                         "4hr", "250C", file_name="4hr_SCR_250C_breakthrough.txt")
+sim.print_results_of_location(["NH3","NO","NO2","N2O","O2","N2","H2O"],
+                                        "4hr", "250C", 0, file_name="4hr_SCR_250C_bypass.txt")
 sim.print_results_of_integral_average(["q1","q2a","q2b","q3a","q3b","q3c"],
                                         "4hr", "250C", file_name="4hr_SCR_250C_average_ads.txt")
 
 sim.print_results_of_breakthrough(["NH3","NO","NO2","N2O","O2","N2","H2O"],
                                         "8hr", "250C", file_name="8hr_SCR_250C_breakthrough.txt")
+sim.print_results_of_location(["NH3","NO","NO2","N2O","O2","N2","H2O"],
+                                        "8hr", "250C", 0, file_name="8hr_SCR_250C_bypass.txt")
 sim.print_results_of_integral_average(["q1","q2a","q2b","q3a","q3b","q3c"],
                                         "8hr", "250C", file_name="8hr_SCR_250C_average_ads.txt")
 
 sim.print_results_of_breakthrough(["NH3","NO","NO2","N2O","O2","N2","H2O"],
                                         "16hr", "250C", file_name="16hr_SCR_250C_breakthrough.txt")
+sim.print_results_of_location(["NH3","NO","NO2","N2O","O2","N2","H2O"],
+                                        "16hr", "250C", 0, file_name="16hr_SCR_250C_bypass.txt")
 sim.print_results_of_integral_average(["q1","q2a","q2b","q3a","q3b","q3c"],
                                         "16hr", "250C", file_name="16hr_SCR_250C_average_ads.txt")
 
