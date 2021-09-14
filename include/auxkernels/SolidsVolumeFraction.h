c5c9aeb6f1efbda6dbf2bc4aef4ed8797be5418d
Austin Ladshaw <ladshawap@ornl.gov>
2021-09-14 10:49:48 -0400

    Added automatic calculation of solids fraction from porosity

---
+++ include/auxkernels/SolidsVolumeFraction.h
@@ -0,0 +1,62 @@
+/*!
+ *  \file SolidsVolumeFraction.h
+ *    \brief Auxillary kernel to calculate the solids volume fraction
+ *    \details This file is responsible for calculating the solids volume fraction
+ *            based on the system bulk porosity. Solids fraction is just (1-eb),
+ *            however, this kernel will be needed if the system bulk porosity (eb)
+ *            varies with space or time, thus, connecting and automating the
+ *            calculation of the complimentary fraction.
+ *
+ *  \author Austin Ladshaw
+ *  \date 09/14/2021
+ *  \copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in catalyst
+ *              performance for new vehicle technologies.
+ *
+ *               Austin Ladshaw does not claim any ownership or copyright to the
+ *               MOOSE framework in which these kernels are constructed, only
+ *               the kernels themselves. The MOOSE framework copyright is held
+ *               by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#pragma once
+
+#include "AuxKernel.h"
+
+/// SolidsVolumeFraction class inherits from AuxKernel
+/** This class object creates an AuxKernel for use in the MOOSE framework. The AuxKernel will
+    calculate the variable as the complimentary ratio to the bulk porosity of the system */
+class SolidsVolumeFraction : public AuxKernel
+{
+public:
+    /// Required new syntax for InputParameters
+    static InputParameters validParams();
+
+    /// Standard MOOSE public constructor
+    SolidsVolumeFraction(const InputParameters & parameters);
+
+protected:
+    /// Required MOOSE function override
+    /** This is the function that is called by the MOOSE framework when a calculation of the total
+        system pressure is needed. You are required to override this function for any inherited
+        AuxKernel. */
+    virtual Real computeValue() override;
+
+private:
+    const VariableValue & _bulk_porosity;       ///< Variable for bulk porosity
+
+};
---
+++ src/auxkernels/SolidsVolumeFraction.C
@@ -0,0 +1,57 @@
+/*!
+ *  \file SolidsVolumeFraction.h
+ *    \brief Auxillary kernel to calculate the solids volume fraction
+ *    \details This file is responsible for calculating the solids volume fraction
+ *            based on the system bulk porosity. Solids fraction is just (1-eb),
+ *            however, this kernel will be needed if the system bulk porosity (eb)
+ *            varies with space or time, thus, connecting and automating the
+ *            calculation of the complimentary fraction.
+ *
+ *  \author Austin Ladshaw
+ *  \date 09/14/2021
+ *  \copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in catalyst
+ *              performance for new vehicle technologies.
+ *
+ *               Austin Ladshaw does not claim any ownership or copyright to the
+ *               MOOSE framework in which these kernels are constructed, only
+ *               the kernels themselves. The MOOSE framework copyright is held
+ *               by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "SolidsVolumeFraction.h"
+
+registerMooseObject("catsApp", SolidsVolumeFraction);
+
+InputParameters SolidsVolumeFraction::validParams()
+{
+    InputParameters params = AuxKernel::validParams();
+    params.addRequiredCoupledVar("porosity","Bulk porosity of the reactor system ");
+    return params;
+}
+
+SolidsVolumeFraction::SolidsVolumeFraction(const InputParameters & parameters) :
+AuxKernel(parameters),
+_bulk_porosity(coupledValue("porosity"))
+{
+
+}
+
+Real SolidsVolumeFraction::computeValue()
+{
+      return (1 - _bulk_porosity[_qp]);
+}
