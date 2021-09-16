187a672ea0594245a72ac446ef6abf2cb2352b11
Austin Ladshaw <ladshawap@ornl.gov>
2021-09-16 11:02:06 -0400

    Added aux kernel to calculate the effective thickness for microscale simulations of monoliths

---
+++ include/auxkernels/MonolithMicroscaleTotalThickness.h
@@ -0,0 +1,80 @@
+/*!
+ *  \file MonolithMicroscaleTotalThickness.h
+ *    \brief Auxillary kernel to calculate the 1D microscale thickness for monoliths
+ *    \details This file is responsible for calculating the effective 1D thickness
+ *              for monoliths to be used in the Microscale set of kernels to resolve
+ *              the intralayer diffusion equations using the hybrid FD/FE method
+ *              of CATS. It should be noted that this is an effective thickness
+ *              and not a true thickness. This effective thickness needs to be
+ *              used to help account for diffusion in all walls and corners. However,
+ *              due to how the Microscale kernels are implemented, you cannot
+ *              automatically integrate this calculation into those kernels yet. Also,
+ *              the units you get from this kernel will align with the units you
+ *              give on input (i.e., if your cell density is in units of # cells
+ *              per cm^2, then the thickness will be in cm).
+ *
+ *  \note The 'micro_length' parameter for Microscale kernels is implemented as a
+ *        Param and not a Var. This is because you cannot place Vars in the
+ *        [GlobalParams] block of an input file. Changing this convention would
+ *        require a substaintial redesign of the user interface for Microscale
+ *        kernels, which is why this change is not being made. User's who wish
+ *        to use the Microscale kernels for a monolith domain should first
+ *        calculate this value in a separate input file, then use that calculated
+ *        value as the 'micro_length' Param in [GlobalParams] for evaluation of
+ *        Microscale kinetics for monoliths. 
+ *
+ *
+ *  \author Austin Ladshaw
+ *  \date 09/16/2021
+ *  \copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in catalyst
+ *              performance for new vehicle technologies.
+ *
+ *               Austin Ladshaw does not claim any ownership or copyright to the
+ *               MOOSE framework in which these kernels are constructed, only
+ *               the kernels themselves. The MOOSE framework copyright is held
+ *               by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#pragma once
+
+#include "AuxKernel.h"
+
+/// MonolithMicroscaleTotalThickness class inherits from AuxKernel
+/** This class object creates an AuxKernel for use in the MOOSE framework. The AuxKernel will
+    calculate the variable given information such as cell density and bulk channel space. */
+class MonolithMicroscaleTotalThickness : public AuxKernel
+{
+public:
+    /// Required new syntax for InputParameters
+    static InputParameters validParams();
+
+    /// Standard MOOSE public constructor
+    MonolithMicroscaleTotalThickness(const InputParameters & parameters);
+
+protected:
+    /// Required MOOSE function override
+    /** This is the function that is called by the MOOSE framework when a calculation of the total
+        system pressure is needed. You are required to override this function for any inherited
+        AuxKernel. */
+    virtual Real computeValue() override;
+
+private:
+    Real _cell_density;        ///< Cell density of the monolith (cells per total face area)
+    const VariableValue & _bulk_porosity;       ///< Ratio of channel volume to total volume
+
+};
---
+++ src/auxkernels/MonolithMicroscaleTotalThickness.C
@@ -0,0 +1,82 @@
+/*!
+ *  \file MonolithMicroscaleTotalThickness.h
+ *    \brief Auxillary kernel to calculate the 1D microscale thickness for monoliths
+ *    \details This file is responsible for calculating the effective 1D thickness
+ *              for monoliths to be used in the Microscale set of kernels to resolve
+ *              the intralayer diffusion equations using the hybrid FD/FE method
+ *              of CATS. It should be noted that this is an effective thickness
+ *              and not a true thickness. This effective thickness needs to be
+ *              used to help account for diffusion in all walls and corners. However,
+ *              due to how the Microscale kernels are implemented, you cannot
+ *              automatically integrate this calculation into those kernels yet. Also,
+ *              the units you get from this kernel will align with the units you
+ *              give on input (i.e., if your cell density is in units of # cells
+ *              per cm^2, then the thickness will be in cm).
+ *
+ *  \note The 'micro_length' parameter for Microscale kernels is implemented as a
+ *        Param and not a Var. This is because you cannot place Vars in the
+ *        [GlobalParams] block of an input file. Changing this convention would
+ *        require a substaintial redesign of the user interface for Microscale
+ *        kernels, which is why this change is not being made. User's who wish
+ *        to use the Microscale kernels for a monolith domain should first
+ *        calculate this value in a separate input file, then use that calculated
+ *        value as the 'micro_length' Param in [GlobalParams] for evaluation of
+ *        Microscale kinetics for monoliths.
+ *
+ *
+ *  \author Austin Ladshaw
+ *  \date 09/16/2021
+ *  \copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in catalyst
+ *              performance for new vehicle technologies.
+ *
+ *               Austin Ladshaw does not claim any ownership or copyright to the
+ *               MOOSE framework in which these kernels are constructed, only
+ *               the kernels themselves. The MOOSE framework copyright is held
+ *               by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "MonolithMicroscaleTotalThickness.h"
+
+registerMooseObject("catsApp", MonolithMicroscaleTotalThickness);
+
+InputParameters MonolithMicroscaleTotalThickness::validParams()
+{
+    InputParameters params = AuxKernel::validParams();
+    params.addParam<Real>("cell_density",50,"Cell density of the monolith (# of cells per face area)");
+    params.addRequiredCoupledVar("channel_vol_ratio","Ratio of channel volume to total volume ");
+    return params;
+}
+
+MonolithMicroscaleTotalThickness::MonolithMicroscaleTotalThickness(const InputParameters & parameters) :
+AuxKernel(parameters),
+_cell_density(getParam<Real>("cell_density")),
+_bulk_porosity(coupledValue("channel_vol_ratio"))
+{
+
+}
+
+Real MonolithMicroscaleTotalThickness::computeValue()
+{
+    Real Ac = _bulk_porosity[_qp]/_cell_density;
+    Real dc = 2.0*sqrt((Ac/3.14159));
+    Real ds = sqrt(Ac);
+    Real dh = 0.5*(dc+ds);
+    Real A1c = 1.0/_cell_density;
+    Real wt = sqrt(A1c - dh*dh);
+    return wt;
+}
