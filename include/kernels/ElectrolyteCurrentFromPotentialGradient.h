5706c2d33c7eae1a7c4643f27de7a5fb092fffe6
Austin Ladshaw <ladshawap@ornl.gov>
2021-11-03 10:07:05 -0400

    Started a set of kernels for e-current

---
+++ include/kernels/ElectrolyteCurrentFromPotentialGradient.h
@@ -0,0 +1,108 @@
+/*!
+ *  \file ElectrolyteCurrentFromPotentialGradient.h
+ *	\brief Standard kernel for coupling a gradient of potential to the formation of current
+ *	\details This file creates a standard MOOSE kernel for the coupling of a non-linear variable
+ *            gradient for electrolyte potential with variables for ion concentration, diffusion
+ *            coefficients for ions, temperature, and ion valence. In the case of anisotopic
+ *            diffusion, the diffusion coefficent the user provides should correspond to the
+ *            direction of the electrolyte current this kernel acts on.
+ *
+ *            Ref:  J.R. Clausen, V.E. Brunini, H.K. Moffat, M.J. Martinez, "Numerical Modeling
+ *                  of an All Vanadium Redox Flow Battery", Sandia Report, SAND2014-0190,
+ *                  Sandia National Laboratories, Albuquerque, NM, January 2014.
+ *
+ *  \note Users MUST provide the direction of the current vector being calculated (0=>x, 1=>y, 2=>z)
+ *
+ *  \author Austin Ladshaw
+ *	\date 11/03/2021
+ *	\copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in electrochemical
+ *              CO2 conversion.
+ *
+ *			   Austin Ladshaw does not claim any ownership or copyright to the
+ *			   MOOSE framework in which these kernels are constructed, only
+ *			   the kernels themselves. The MOOSE framework copyright is held
+ *			   by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#pragma once
+
+#include "Kernel.h"
+
+/// ElectrolyteCurrentFromPotentialGradient class object inherits from Kernel object
+/** This class object inherits from the Kernel object in the MOOSE framework.
+	All public and protected members of this class are required function overrides.
+	The kernel has a vector whose components can be set piecewise in an
+	input file. */
+class ElectrolyteCurrentFromPotentialGradient : public Kernel
+{
+public:
+  /// Required new syntax for InputParameters
+  static InputParameters validParams();
+
+	/// Required constructor for objects in MOOSE
+	ElectrolyteCurrentFromPotentialGradient(const InputParameters & parameters);
+
+protected:
+  /// Helper function to formulate the sum of ion terms
+  Real sum_ion_terms();
+
+  /// Helper function to formulate the full coupled coefficent
+  Real effective_ionic_conductivity();
+
+	/// Required residual function for standard kernels in MOOSE
+	/** This function returns a residual contribution for this object.*/
+	virtual Real computeQpResidual() override;
+
+	/// Required Jacobian function for standard kernels in MOOSE
+	/** This function returns a Jacobian contribution for this object. The Jacobian being
+		computed is the associated diagonal element in the overall Jacobian matrix for the
+		system and is used in preconditioning of the linear sub-problem. */
+	virtual Real computeQpJacobian() override;
+
+  /// Not Required, but aids in the preconditioning step
+  /** This function returns the off diagonal Jacobian contribution for this object. By
+        returning a non-zero value we will hopefully improve the convergence rate for the
+        cross coupling of the variables. */
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+
+	RealVectorValue _norm_vec;	    ///< Vector for direction of gradient
+
+  unsigned int _dir;				      ///< Direction of current this kernel acts on (0=x, 1=y, 2=z)
+
+  const VariableGradient & _e_potential_grad;            ///< Coupled eletric potential variable (V or J/C)
+  const unsigned int _e_potential_var;                   ///< Variable identification for coupled eletric potential
+
+  const VariableValue & _porosity;			  ///< Porosity variable
+  const unsigned int _porosity_var;				///< Variable identification for porosity
+
+  const VariableValue & _temp;			  ///< Temperature variable (K)
+  const unsigned int _temp_var;				///< Variable identification for temperature
+
+  Real _faraday;                      ///< Value of Faraday's Constant (default = 96485.3 C/mol)
+  Real _gas_const;                    ///< Value of the Gas law constant (default = 8.314462 J/K/mol)
+
+  std::vector<const VariableValue *> _ion_conc;           ///< Pointer list to the coupled ion concentrations (mol/L^3)
+  std::vector<const VariableValue *> _diffusion;          ///< Pointer list to the coupled diffusion coeffs (L^2/T)
+  std::vector<unsigned int> _ion_conc_vars;               ///< Indices for the coupled reactants
+  std::vector<unsigned int> _diffusion_vars;              ///< Indices for the coupled products
+
+  std::vector<Real> _valence;                             ///< Valence list for corresponding ions
+
+private:
+
+};
---
+++ src/kernels/ElectrolyteCurrentFromPotentialGradient.C
@@ -0,0 +1,161 @@
+/*!
+ *  \file ElectrolyteCurrentFromPotentialGradient.h
+ *	\brief Standard kernel for coupling a gradient of potential to the formation of current
+ *	\details This file creates a standard MOOSE kernel for the coupling of a non-linear variable
+ *            gradient for electrolyte potential with variables for ion concentration, diffusion
+ *            coefficients for ions, temperature, and ion valence. In the case of anisotopic
+ *            diffusion, the diffusion coefficent the user provides should correspond to the
+ *            direction of the electrolyte current this kernel acts on.
+ *
+ *            Ref:  J.R. Clausen, V.E. Brunini, H.K. Moffat, M.J. Martinez, "Numerical Modeling
+ *                  of an All Vanadium Redox Flow Battery", Sandia Report, SAND2014-0190,
+ *                  Sandia National Laboratories, Albuquerque, NM, January 2014.
+ *
+ *  \note Users MUST provide the direction of the current vector being calculated (0=>x, 1=>y, 2=>z)
+ *
+ *  \author Austin Ladshaw
+ *	\date 11/03/2021
+ *	\copyright This kernel was designed and built at Oak Ridge National
+ *              Laboratory by Austin Ladshaw for research in electrochemical
+ *              CO2 conversion.
+ *
+ *			   Austin Ladshaw does not claim any ownership or copyright to the
+ *			   MOOSE framework in which these kernels are constructed, only
+ *			   the kernels themselves. The MOOSE framework copyright is held
+ *			   by the Battelle Energy Alliance, LLC (c) 2010, all rights reserved.
+ */
+
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "ElectrolyteCurrentFromPotentialGradient.h"
+
+/**
+ * All MOOSE based object classes you create must be registered using this macro.  The first
+ * argument is the name of the App with an "App" suffix (i.e., "fennecApp"). The second
+ * argument is the name of the C++ class you created.
+ */
+registerMooseObject("catsApp", ElectrolyteCurrentFromPotentialGradient);
+
+InputParameters ElectrolyteCurrentFromPotentialGradient::validParams()
+{
+    InputParameters params = Kernel::validParams();
+    params.addRequiredParam<unsigned int>("direction","Directional index for current that this kernel acts on (0 = x, 1 = y, 2 = z)");
+
+    params.addRequiredCoupledVar("electric_potential","Variable for electric potential (V or J/C)");
+    params.addCoupledVar("porosity",1,"Variable for volume fraction or porosity (default = 1)");
+    params.addCoupledVar("temperature",298,"Variable for temperature of the media (default = 298 K)");
+
+    params.addParam<Real>("faraday_const",96485.3, "Value of Faraday's constant (default = 96485.3 C/mol)");
+    params.addParam<Real>("gas_const",8.314462, "Value of the gas law constant (default = 8.314462 J/K/mol)");
+
+    params.addRequiredCoupledVar("ion_conc","List of names of the ion concentration variables (mol/L^3)");
+    params.addRequiredCoupledVar("diffusion","List of names of the diffusion variables (L^2/T)");
+    params.addParam< std::vector<Real> >("ion_valence","List of valences for coupled ion concentrations");
+    return params;
+}
+
+ElectrolyteCurrentFromPotentialGradient::ElectrolyteCurrentFromPotentialGradient(const InputParameters & parameters) :
+Kernel(parameters),
+_dir(getParam<unsigned int>("direction")),
+
+_e_potential_grad(coupledGradient("electric_potential")),
+_e_potential_var(coupled("electric_potential")),
+_porosity(coupledValue("porosity")),
+_porosity_var(coupled("porosity")),
+_temp(coupledValue("porosity")),
+_temp_var(coupled("porosity")),
+
+_faraday(getParam<Real>("faraday_const")),
+_gas_const(getParam<Real>("gas_const")),
+
+_valence(getParam<std::vector<Real> >("ion_valence"))
+
+{
+    if (_dir > 2 || _dir < 0)
+    {
+        moose::internal::mooseErrorRaw("Invalid current direction index!");
+    }
+
+    _norm_vec(0) = 0.0;
+    _norm_vec(1) = 0.0;
+    _norm_vec(2) = 0.0;
+    _norm_vec(_dir) = 1.0;
+
+    unsigned int c = coupledComponents("ion_conc");
+    _ion_conc_vars.resize(c);
+    _ion_conc.resize(c);
+
+    unsigned int d = coupledComponents("diffusion");
+    _diffusion_vars.resize(d);
+    _diffusion.resize(d);
+
+    //Check lists to ensure they are of same size
+    if (c != d)
+    {
+      moose::internal::mooseErrorRaw("User is required to provide list of ion concentration variables of the same length as list of diffusion coefficients.");
+    }
+    if (_ion_conc_vars.size() != _valence.size())
+    {
+      moose::internal::mooseErrorRaw("User is required to provide list of ion concentration variables of the same length as list of ion valences.");
+    }
+
+    if (_diffusion_vars.size() != _valence.size())
+    {
+      moose::internal::mooseErrorRaw("User is required to provide list of diffusion variables of the same length as list of ion valences.");
+    }
+
+    //Grab the variables
+    for (unsigned int i = 0; i<_ion_conc.size(); ++i)
+    {
+        _ion_conc_vars[i] = coupled("ion_conc",i);
+        _ion_conc[i] = &coupledValue("ion_conc",i);
+    }
+
+    for (unsigned int i = 0; i<_diffusion.size(); ++i)
+    {
+        _diffusion_vars[i] = coupled("diffusion",i);
+        _diffusion[i] = &coupledValue("diffusion",i);
+    }
+}
+
+Real ElectrolyteCurrentFromPotentialGradient::sum_ion_terms()
+{
+    Real sum = 0.0;
+    for (unsigned int i = 0; i<_ion_conc.size(); ++i)
+    {
+        sum = sum + _valence[i]*_valence[i]*(*_diffusion[i])[_qp]*(*_ion_conc[i])[_qp];
+    }
+    return sum;
+}
+
+Real ElectrolyteCurrentFromPotentialGradient::effective_ionic_conductivity()
+{
+    return (_faraday*_faraday/_gas_const/_temp[_qp])*_porosity[_qp]*sum_ion_terms();
+}
+
+Real ElectrolyteCurrentFromPotentialGradient::computeQpResidual()
+{
+    return -_test[_i][_qp]*effective_ionic_conductivity()*(_norm_vec*_e_potential_grad[_qp]);
+}
+
+Real ElectrolyteCurrentFromPotentialGradient::computeQpJacobian()
+{
+    return 0.0;
+}
+
+Real ElectrolyteCurrentFromPotentialGradient::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  return 0.0;
+}
